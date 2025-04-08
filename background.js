b232d96a0f5716f0a818e8c33861d38bac8dddae
icsd-len-pc <cstomce@gmail.com>
2025-04-08 11:30:45 +0800

    refactor: upgrade manifest file to v3 and refactor all code

--- background.js
+++ background.js
@@ -1,59 +1,50 @@
-//SHDON Website Info DEV
-//V 0.6.0
-//Developed by Cstome CHan (www.icsd.ml)
+chrome.webRequest.onCompleted.addListener(
+  async (details) => {
+    // 仅处理主文档请求（类型为 "main_frame"）
+    if (details.type === "main_frame" && details.ip && details.tabId > -1) {
+      try {
+        const response = await fetch(
+          `http://ip-api.com/json/${details.ip}?lang=zh-CN`
+        );
+        const ipData = await response.json();
 
-var currentIPList = {};	//定义为Object
-// 监听所有已完成的 Web 请求
-chrome.webRequest.onCompleted.addListener(function (info) {
-	//chrome.webRequest用于监听所有网络请求，需要在manifest.json中申请，.onCompleted.addListener指在请求完成时监听，监听到的数据通过info回调给匿名函数处理;
-	// background.js 的console通过插件管理页的inspect查看
-	//console.log(currentIPList);	//调试查看 currentIPList 格式
-	//console.log(info);			//调试查看 info 格式
-	currentIPList[info.url] = [info.ip, info.fromCache, info.requestId, info.statusLine, info.tabId];
-	//将监听到的所有网络请求以"url":["ip地址","是否来自缓存","本次请求ID","例：HTTP/1.1 204","标签ID"] 保存到该Object;
-	return;
-}, {urls: [], types: []}/*不可省的未知含义参数*/);
+        // 构造数据对象
+        const url = new URL(details.url); // 获取 URL 对象
+        const data = {
+          domain: url.hostname, // 提取域名
+          ip: details.ip,
+          region: ipData
+            ? `${ipData.regionName}, ${ipData.country}`
+            : "Unknown",
+          fromCache: details.fromCache,
+          requestId: details.requestId,
+          statusLine: details.statusLine,
+          tabId: details.tabId,
+        };
 
-// 接受标签页发送的信息查询请求
-chrome.extension.onMessage.addListener( function (request, sender, sendResponse) {
-	//监听页面中注入的main.js中的chrome.extension.sendMessage发送的消息
-	//console.log(sender);
+        // 检查目标标签页是否可以接收消息
+        chrome.tabs.sendMessage(
+          details.tabId,
+          { action: "ping" },
+          (response) => {
+            if (chrome.runtime.lastError || !response) {
+              console.warn(
+                `Tab ${details.tabId} is not available for messaging.`
+              );
+              return; // 无法发送消息，跳过
+            }
 
-	sendResponse({}) // 回信：空
-
-	var currentURL = sender.tab.url;	//定义为发送信息的页面（标签）的url
-	if (currentIPList[currentURL] !== undefined) {
-		//在currentIPList中查找是否有与标签url相同的记录
-		currentIPList[currentURL].push(sender.tab.url);
-
-		let wsInfo = currentIPList[currentURL];
-		// 查询IP信息后发送结果给标签页
-		getIpInfo(wsInfo[0]).then(ipInfo => {
-			chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-				chrome.tabs.sendMessage(wsInfo[4], {
-					//有则返回Object
-					wsInfo,
-					ipInfo
-				}, function(response) {
-				});
-			  });
-		})
-		currentIPList = {} //当匹配到URL对应的信息后，清空保存的网络请求信息
-	}
-});
-
-
-function getIpInfo(ip) {
-	let ipInfo = new XMLHttpRequest;
-	return new Promise((resolve, reject) => {
-		ipInfo.onreadystatechange = function() {
-			if(ipInfo.readyState == 4 && ipInfo.status == 200){
-				let ipInfoObj = JSON.parse(ipInfo.responseText);
-				resolve (ipInfoObj)
-			}
-		}
-		// ipInfo.open('GET',`https://api.nettool.app/ip/?ip=${ip}`, true);
-		ipInfo.open('GET',`http://ip-api.com/json/${ip}?lang=zh-CN`, true);
-		ipInfo.send();
-	})
-}
+            // 发送真正的数据
+            chrome.tabs.sendMessage(details.tabId, {
+              action: "showInfo",
+              data,
+            });
+          }
+        );
+      } catch (error) {
+        console.error("Error fetching IP data:", error);
+      }
+    }
+  },
+  { urls: ["<all_urls>"] }
+);
--- websiteinfo.js
+++ websiteinfo.js
@@ -1,50 +1,33 @@
-// 页面打开后，发送请求到 background.js 请求相关信息
-chrome.extension.sendMessage({}/*发送信息的内容，这里为空Object，但不能没有*/, function (response) {
-	//匿名函数处理background.js返回的数据，通过参数response传递
+// 响应 ping 消息
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "ping") {
+    sendResponse({ status: "ready" });
+    return; // 确认内容脚本已加载
+  }
+
+  if (message.action === "showInfo" && message.data) {
+    const info = message.data;
+
+    // 创建并显示信息框
+    const infoBox = document.createElement("div");
+    infoBox.id = "website-info-box";
+    infoBox.className = "snotice";
+    infoBox.innerHTML = `
+      <div><strong>Domain:</strong> ${info.domain}
+	 <a href="https://whois.chinaz.com/${info.domain}" target="_blank">whois</a>
+	  </div>
+      <div><strong>IP:</strong> ${info.ip}</div>
+      <div><strong>Region:</strong> ${info.region}</div>
+      <div><strong>From Cache:</strong> ${info.fromCache}</div>
+      <div><strong>Request ID:</strong> ${info.requestId}</div>
+      <div><strong>Status Line:</strong> ${info.statusLine}</div>
+      <div><strong>Tab ID:</strong> ${info.tabId}</div>
+    `;
+    document.body.appendChild(infoBox);
+
+    // 设置5秒后自动移除;
+    setTimeout(() => {
+      infoBox.remove();
+    }, 5000);
+  }
 });
-
-// 接受 background.js 传来的相关信息
-chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
-	console.log(request);
-	displayInfo(request);
-	sendResponse({})
-});
-
-//SHDON Notice 模块
-function showNotice(c, t) {
-	var ntdiv = document.createElement("div");
-	ntts = new Date().getTime();
-	ntdiv.className = "snotic"
-	ntdiv.id = "snotic" + ntts;
-	ntdiv.innerHTML = c;
-	document.body.appendChild(ntdiv);
-	var setTimeoutCode = "document.body.removeChild(document.getElementById('snotic"+ntts+"'))";
-	setTimeout(setTimeoutCode, t);
-}
-
-//urlToDomain 模块 v0.2.1
-function urlToDomain(url) {
-	var reg = new RegExp("//.*?/", "i");
-	var domain = url.match(reg)[0];
-	domain = domain.replace(/[\/]/g, "");
-	return domain;
-}
-
-//display responsed data
-function displayInfo(r) {
-	let wsInfo = r.wsInfo;
-	let ipInfo = r.ipInfo;
-
-	let t = `
-		Domain: ${urlToDomain(wsInfo[5])}
-		<a target=\"_blank\" href=\"http://whois.chinaz.com/${urlToDomain(wsInfo[5])}\">Whois</a>
-		<br>IP: ${wsInfo[0]}
-		<br>IP Area: ${ipInfo.country} ${ipInfo.regionName} ${ipInfo.city}
-		<br>AS: ${ipInfo.as}
-		<br>fromCache: ${wsInfo[1]}
-		<br>requestId: ${wsInfo[2]}
-		<br>statusLine: ${wsInfo[3]}
-		<br>tabId: ${wsInfo[4]}`;
-
-	showNotice(t, 5000);
-}
